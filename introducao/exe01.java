8ba800b7d18fa97914a2a2f5817e71c5dcd913f5
Dionatan Rodrigues <48455774+Dionatan2019Rodrigues@users.noreply.github.com>
2023-03-21 12:58:10 -0300

    Create exe01.java

---
+++ introducao/exe01.java
@@ -0,0 +1,8 @@
+public class OperadoresAritmeticos {
+    public static void main(String[] args){
+        int i = 28;
+        int j = 43;
+        int res = i * j;
+        System.out.println("Produto: " + res);
+    }
+}
